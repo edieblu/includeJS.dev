6a387375ec2467463a49c41c847b7b672b1bc8b1
EVA <edie.blues@gmail.com>
2019-12-20 18:42:20 +0100

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,19 +4,17 @@ import Layout from '../components/layout';
 export default () => (
   <Layout>
     <h1>Contact us</h1>
-    <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+    <form name="Contact Form" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="Contact Form" />
+      <div>
+        <label>Your Email:</label>
+        <input type="email" name="email" />
+      </div>
+      <div>
+        <label>Message:</label>
+        <textarea name="message" />
+      </div>
+      <button type="submit">Send</button>
     </form>
   </Layout>
 )
