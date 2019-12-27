ece90ae789e6e73a7b476173ce7ca6917d9914b1
EVA <edie.blues@gmail.com>
2019-12-27 10:25:43 +0100

    change tagline

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import Layout from '../components/layout';
-import logo from '../../static/logo.svg'
+import logo from '../../static/logo.svg';
 import { css } from '@emotion/core';
 
 export default () => (
   <Layout>
-    <img src={logo} alt='Logo' css={css`
-    margin-left: -53px;
-    margin-bottom: -30px;
-    `}/>
-    <h3>A beginner-friendly JavaScript community, for minorities in tech.</h3>
+    <img
+      src={logo}
+      alt="Logo"
+      css={css`
+        margin-left: -53px;
+        margin-bottom: -30px;
+      `}
+    />
+    <h3>A beginner-friendly JavaScript community.</h3>
+    <br/>
+    <p>
+      <strong>includeJS aims to bridge the diversity gap in tech.</strong> We're starting by
+      concentrating on women in tech. As we get more established, we'll expand
+      our focus to include other groups.
+    </p>
 
-    <p>Our first event will be hosted on January 16th at <strong><a href='https://www.anais.digital/index'>Anais Digital</a></strong>, Boulevard Brand Whitlock 87</p>
+    <p>
+      Our first event will be hosted on January 16th at{' '}
+      <strong>
+        <a href="https://www.anais.digital/index">Anais Digital</a>
+      </strong>
+      , Boulevard Brand Whitlock 87
+    </p>
 
-    <p>For more details and RSVP, head over <a href="https://www.meetup.com/includeJS-bridge-the-diversity-gap/">our meetup page.</a></p>
+    <p>
+      For more details and RSVP, head over{' '}
+      <a href="https://www.meetup.com/includeJS-bridge-the-diversity-gap/">
+        our meetup page.
+      </a>
+    </p>
 
-    <p>If you would like to joins us as a coach or sponsor, get in touch with us on <a href='https://twitter.com/GirlsCodeMK'>Twitter</a>.</p>
-
+    <p>
+      If you would like to joins us as a coach or sponsor, get in touch with us
+      on <a href="https://twitter.com/GirlsCodeMK">Twitter</a>.
+    </p>
   </Layout>
 );
