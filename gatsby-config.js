251bbbd65842725652dbb9b4920f19f729eda5e4
EVA <edie.blues@gmail.com>
2020-03-10 20:41:12 +0100

    WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,5 +19,13 @@ module.exports = {
       path: `${__dirname}/src/posts`,
     },
   },
+  {
+    resolve: 'gatsby-plugin-mdx',
+    options: {
+      defaultLayouts: {
+        default: require.resolve('./src/components/layout.js'),
+      },
+    },
+  },
 ],
 }
