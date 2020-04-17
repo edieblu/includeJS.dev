cd9f6ba8081c682a177aadeb3e5e07e429cd7c77
EVA <edie.blues@gmail.com>
2020-04-17 18:58:59 +0200

    new videos

--- src/pages/video.js
+++ src/pages/video.js
@@ -20,6 +20,32 @@ export default () => (
 
     <h4>Watch recordings:</h4>
     <ul>
+
+    <li>
+        <h5>
+          Eva and Kelly talk dev, podcasting, entrepreneurship and funny mugs!
+          <br></br>
+          April 17, 2020 (
+          <StyledA href="https://www.youtube.com/watch?v=GQiFycJOOT0">
+            YouTube Link
+          </StyledA>
+          )
+        </h5>
+      </li>
+
+      <li>
+        <h5>
+        Eva and Swyx talk #LearnInPublic, teaching and career change!
+          <br></br>
+          April 14, 2020 (
+          <StyledA href="https://www.youtube.com/watch?v=1ayI9j57VlQ">
+            YouTube Link
+          </StyledA>
+          )
+        </h5>
+      </li>
+
+
       <li>
         <h5>
           Title: Eva and Anjana talk Career change, web development and karaoke. 🎤
